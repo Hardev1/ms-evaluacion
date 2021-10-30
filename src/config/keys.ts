3beeef744f0f606bea77ae3107a8af71d176049c
BryanMauricioG <72142231+BryanMauricioG@users.noreply.github.com>
2021-10-30 15:44:48 -0500

    Add files via upload

---
+++ src/config/keys.ts
@@ -0,0 +1,9 @@
+export namespace Keys {
+  export const carpetaImagenProponente = "../../archivos/proponentes/";
+  export const nombreCampoImagenProponente = 'file';
+  export const extensionesPermitidasIMG: string[] = ['.PNG', '.JPG', '.JPEG', '.SVG'];
+  export const tamMaxImagenProponente = 1024 * 1024;
+  export const carpetaDocumentoPersona = '../../archivos/documentos';
+  export const nombreCampoDocumentoPersona = 'file';
+  export const extensionesPermitidasDOC: string[] = ['.PDF', '.DOC', '.DOCX', '.XLS', '.XLSX'];
+}
